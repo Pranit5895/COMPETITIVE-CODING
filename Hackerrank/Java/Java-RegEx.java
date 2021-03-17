e307c4a92158e37d38f99d2312a9835c7d259de9
Pranit5895 <guptapranit9@gmail.com>
2021-03-17 14:20:10 +0530

    Java RegEx

---
+++ Hackerrank/Java/Java-RegEx.java
@@ -0,0 +1,12 @@
+
+
+//Write your code here
+/*
+     [01]?\\d{1,2}   matches numbers 0-199.
+     2[0-4]\\d       matches numbers 200-249
+     25[0-5]         matches numbers 250-255
+*/
+class MyRegex {
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." +  num + "." +  num + "." + num;
+}
