cf0459a0c6f44162031d8a76bc4c040d91fa953c
Pranit5895 <guptapranit9@gmail.com>
2021-03-22 16:47:06 +0530

    Java Hashset

---
+++ Hackerrank/Java/Java-Hashset.java
@@ -0,0 +1,11 @@
+
+
+//Write your code here
+s.close();
+
+        HashSet<String> set = new HashSet(t);
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }
+
