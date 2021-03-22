fba5275d7626f0a7a5025dba9431b6103b81c059
Pranit5895 <guptapranit9@gmail.com>
2021-03-22 16:47:44 +0530

    Java Generics

---
+++ Hackerrank/Java/Java-Generics.java
@@ -0,0 +1,13 @@
+
+
+class Printer
+{
+   //Write your code here
+   <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+ 
+}
+
