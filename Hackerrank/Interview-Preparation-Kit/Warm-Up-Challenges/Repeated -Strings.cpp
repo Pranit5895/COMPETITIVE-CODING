fd439d9d3feaf8e99c4f2385e857fc5faffe2540
Pranit5895 <guptapranit9@gmail.com>
2021-04-22 23:40:33 +0530

    Repeated Strings

---
+++ Hackerrank/Interview-Preparation-Kit/Warm-Up-Challenges/Repeated -Strings.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+long long count(string const & str){
+    long long ans = 0;
+    for (const char & c: str){
+        ans += c == 'a';
+    }
+    return ans;
+}
+
+
+int main(){
+    string str;
+    cin >> str;
+
+    long long n;
+    cin >> n;
+
+    long long ans = 0;
+    ans = n / str.size() * count(str) + count(str.substr(0, n % str.size()));
+
+    cout << ans;
+    return 0;
+}
