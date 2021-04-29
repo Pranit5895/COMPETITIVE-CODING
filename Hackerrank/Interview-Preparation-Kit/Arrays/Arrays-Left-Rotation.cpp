402cd37ade86b3bfc9aa94e799af84fedaaa54aa
Pranit5895 <guptapranit9@gmail.com>
2021-04-29 20:11:00 +0530

    Arrays Left Rotation

---
+++ Hackerrank/Interview-Preparation-Kit/Arrays/Arrays-Left-Rotation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n, d;
+
+    cin >> n;
+    cin >> d;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++){
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << arr[(i + d) % n] << " ";
+    }
+
+    return 0;
+}
