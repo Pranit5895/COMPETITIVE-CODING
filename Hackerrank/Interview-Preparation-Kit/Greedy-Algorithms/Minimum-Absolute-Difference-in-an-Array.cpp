3841af7635b13c6ce1d70b6d769646c0f92681d4
Pranit5895 <guptapranit9@gmail.com>
2021-05-06 19:00:10 +0530

    Minimum-Absolute-Difference-in-an-Array code

---
+++ Hackerrank/Interview-Preparation-Kit/Greedy-Algorithms/Minimum-Absolute-Difference-in-an-Array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++){
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int ans = abs(arr[0] - arr[1]);
+    for (int i = 1; i < arr.size(); i++){
+        if (abs(arr[i] - arr[i-1]) < ans){
+            ans = abs(arr[i] - arr[i-1]);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
